0fc07023fe5fe8ad5a37c22b721d105c012d4c5f
Michael Weibel <michael.weibel@gmail.com>
2013-11-25 10:19:41 +0100

    Add IAutoReservationService

---
+++ AutoReservation.Service.Wcf/IAutoReservationService.cs
@@ -0,0 +1,22 @@
+﻿using AutoReservation.Common.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoReservation.Service.Wcf
+{
+    interface IAutoReservationService
+    {
+        public List<DtoBase> findAll();
+
+        public DtoBase findOne(int id);
+
+        public DtoBase insert(DtoBase entry);
+
+        public DtoBase update(DtoBase entry);
+
+        public bool delete(DtoBase entry);
+    }
+}
