fbfc2fe9734a7ebafc96c97b8c818d423fe820ac
Michael Weibel <michael.weibel@gmail.com>
2013-12-02 10:18:56 +0100

    Basic UI Factories (not working)

---
+++ AutoReservation.Ui/Factory/RemoteDataAccessCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoReservation.Ui.Factory
+{
+    class RemoteDataAccessCreator: Creator
+    {
+        public override Common.Interfaces.IAutoReservationService CreateInstance()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
