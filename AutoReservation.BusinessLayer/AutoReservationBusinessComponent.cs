019ac35de8c20684b119139c9f9472e4668f4441
Michael Weibel <michael.weibel@gmail.com>
2013-12-02 09:54:53 +0100

    Business Layer

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -8,8 +8,29 @@ namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
-        #region Find
+        #region FindAll
+        public DbSet<Auto> FindAllAutos()
+        {
+            using (AutoReservationEntities context = new AutoReservationEntities())
+            {
+                return context.Set<Auto>();
+            }
+        }
+        #endregion FindAll
 
+        #region Find
+        public Auto FindAuto(int id)
+        {
+            using (AutoReservationEntities context = new AutoReservationEntities())
+            {
+                return Find<Auto>(context, context.Autos, id);
+            }
+        }
+
+        private T Find<T>(AutoReservationEntities context, DbSet<T> dbSet, int id) where T : class
+        {
+            return dbSet.Find(id);
+        }
         #endregion Find
 
         #region Update
@@ -28,12 +49,14 @@ namespace AutoReservation.BusinessLayer
                 dbSet.Attach(original);
                 context.Entry(original).CurrentValues.SetValues(modified);
                 context.SaveChanges();
+                return modified;
             }
             catch (DbUpdateConcurrencyException)
             {
                 HandleDbConcurrencyException(context, original);
             }
-            return modified;
+            // why ever that's needed.. as it never gets here
+            return null;
         }
         #endregion Update
 
@@ -51,6 +74,31 @@ namespace AutoReservation.BusinessLayer
             return dbSet.Add(entry);
         }
         #endregion Insert
+
+        #region Delete
+        private Auto DeleteAuto(Auto entry)
+        {
+            using (AutoReservationEntities context = new AutoReservationEntities())
+            {
+                return Delete<Auto>(context, context.Autos, entry);
+            }
+        }
+
+        private T Delete<T>(AutoReservationEntities context, DbSet<T> dbSet, T entry) where T : class
+        {
+            try
+            {
+                dbSet.Attach(entry);
+                return dbSet.Remove(entry);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                HandleDbConcurrencyException(context, entry);
+            }
+            // why ever that's needed.. as it never gets here
+            return null;
+        }
+        #endregion Delete
 
         private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
         {
--- AutoReservation.Dal/Properties/Settings1.Designer.cs
+++ AutoReservation.Dal/Properties/Settings1.Designer.cs
@@ -22,5 +22,16 @@ namespace AutoReservation.Dal.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\arsystem.mdf;Integra" +
+            "ted Security=True")]
+        public string Database2ConnectionString {
+            get {
+                return ((string)(this["Database2ConnectionString"]));
+            }
+        }
     }
 }
