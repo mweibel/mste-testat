bb672479eeb4f6e89d57259e90ea44bb70fac06c
Michael Weibel <michael.weibel@gmail.com>
2013-12-08 18:09:56 +0100

    Applied test-wise the flat ui style

--- AutoReservation.Ui/MainWindow.xaml.cs
+++ AutoReservation.Ui/MainWindow.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace AutoReservation.Ui
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
