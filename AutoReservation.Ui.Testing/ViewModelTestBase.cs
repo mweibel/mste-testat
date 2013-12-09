a69e97610047333f64a1c8ac6bae38bccd09d868
Michael Weibel <michael.weibel@gmail.com>
2013-12-09 15:49:20 +0100

    Always run tests remotely

---
+++ AutoReservation.Ui.Testing/ViewModelTestBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading;
+using System.Windows.Input;
+using AutoReservation.Common.Interfaces;
+using AutoReservation.Testing;
+using AutoReservation.Ui.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoReservation.Ui.Testing
+{
+    [TestClass]
+    public abstract class ViewModelTestBase
+    {
+        [TestInitialize]
+        public void InitializeTestData()
+        {
+            TestEnvironmentHelper.InitializeTestData();
+            SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+            var target = Target;
+        }
+
+        protected abstract IAutoReservationService Target
+        {
+            get;
+        }
+
+        [TestMethod]
+        public void AutosLoadTest()
+        {
+            var autoViewModel = new AutoViewModel();
+            var cmd = autoViewModel.LoadCommand;
+            Assert.IsTrue(cmd.CanExecute(this));
+            cmd.Execute(this);
+            Assert.IsNotNull(autoViewModel.Autos);
+        }
+
+        [TestMethod]
+        public void KundenLoadTest()
+        {
+            var kundeViewModel = new KundeViewModel();
+            var cmd = kundeViewModel.LoadCommand;
+            Assert.IsTrue(cmd.CanExecute(this));
+            cmd.Execute(this);
+            Assert.IsNotNull(kundeViewModel.Kunden);
+        }
+
+        [TestMethod]
+        public void ReservationenLoadTest()
+        {
+            var reservationViewModel = new ReservationViewModel();
+            var cmd = reservationViewModel.LoadCommand;
+            Assert.IsTrue(cmd.CanExecute(this));
+            cmd.Execute(this);
+            Assert.IsNotNull(reservationViewModel.Reservationen);
+        }
+    }
+}
---
+++ AutoReservation.Ui.Testing/ViewModelTestRemote.cs
@@ -0,0 +1,39 @@
+﻿using System.ServiceModel;
+using AutoReservation.Common.Interfaces;
+using AutoReservation.Service.Wcf.Host;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoReservation.Service.Wcf;
+
+namespace AutoReservation.Ui.Testing
+{
+    [TestClass]
+    public class ViewModelTestRemote : ViewModelTestBase
+    {
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            AutoReservationServiceHost.StartService();
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            AutoReservationServiceHost.StopService();
+        }
+
+        private IAutoReservationService target;
+        protected override IAutoReservationService Target
+        {
+            get
+            {
+                if (target == null)
+                {
+                    ChannelFactory<IAutoReservationService> channelFactory = new ChannelFactory<IAutoReservationService>("AutoReservationService");
+                    target = channelFactory.CreateChannel();
+                }
+                return target;
+            }
+        }
+
+    }
+}
